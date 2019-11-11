f5cb551ec80e962433b798fbcd6530a6287c4665
Jack Otto <jno003@bucknell.edu>
2019-11-11 11:50:58 -0500

    cards

---
+++ src/Blackjack/Cards.java
@@ -0,0 +1,32 @@
+package Blackjack;
+
+import javax.swing.*;
+
+/**
+ * create an enumeration that holds all of the cards titles and the value of each individual card
+ */
+public enum Cards {
+    ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
+
+    private int value;
+
+    /**
+     * Create a singular card with the given value
+     * @param value - the value of the given card
+     */
+    Cards(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Using a setter for the enum because the value of the ace may change depending on the score of the hand
+     * @param value - the value of the card
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
