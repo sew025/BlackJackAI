5316669b2c2e8a4f61f6886229c8dc8fe3646e1a
Jack Otto <jno003@bucknell.edu>
2019-11-12 15:24:21 -0500

    dealer

---
+++ src/Blackjack/PlayGame.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 11/12/19
+ * Time: 3:22 PM
+ *
+ * Project: csci205finalproject
+ * Package: Blackjack
+ * Class: PlayGame
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package Blackjack;
+
+public class PlayGame {
+    public static void main(String[] args) {
+        Dealer deal = new Dealer()
+    }
+}
