f5496526f38a04ab5ee5529762a3cf4cde7c3baf
sew025 <sew025@bucknell.edu>
2019-11-13 13:29:41 -0500

    "Music to Play in Background of GUI"

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module csci205finalproject {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires java.desktop;
+    requires javafx.media;
+    opens GAGA.backgroundMusic;
+}
