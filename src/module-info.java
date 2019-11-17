67c50f344bb74f9ccaa5c536743541354be93fd9
Mattt McGauley <mwm021@bucknell.edu>
2019-11-17 15:30:22 -0500

    Landing page is done!

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module csci205FinalProject {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires java.desktop;
+    opens LandingPageGUI;
+}
