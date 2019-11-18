67854b6aac43c09f1f1bf6c092103305a87caa96
Jack Otto <jno003@bucknell.edu>
2019-11-18 11:29:29 -0500

    set up the single player gui

---
+++ src/LandingPageGUI/SinglePlayerModel.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 11/18/19
+ * Time: 11:22 AM
+ *
+ * Project: csci205finalproject
+ * Package: LandingPageGUI
+ * Class: SinglePlayerModel
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package LandingPageGUI;
+
+/**
+ * This is the functional part of the GUI, in the model we will be doing everything that requires the game to run and perform the game of blackjack
+ */
+public class SinglePlayerModel {
+}
