03392d7357d50955d78e5ab89768b04e1e5bf3d1
sew025 <sew025@bucknell.edu>
2019-11-29 16:45:25 -0500

    "random solver stuff"

--- src/Blackjack/randomTrackSuccess.java
+++ src/Blackjack/randomTrackSuccess.java
@@ -1,66 +1,53 @@
 package Blackjack;
 
-import Blackjack.Dealer;
-import Blackjack.Player;
-import Blackjack.randomPlayer;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class randomTrackSuccess {
-        private Dealer dealer;
-        private ArrayList<Integer> dealerHand;
-        private int dealerScore;
-        private ArrayList<Integer> playerHand;
-        private int playerScore;
-        private randomPlayer player;
-        private ArrayList<Integer> successes;
-        private int success;
-        private int count;
+    private static ArrayList<Double> successes;
+    private static double success;
+    private static double count;
+    private static int numHands;
 
 
-        public randomTrackSuccess(Dealer dealer, ArrayList<Integer> dealerHand, int dealerScore, ArrayList<Integer> playerHand,
-                            int playerScore, Player player) {
-            this.dealerHand = dealerHand;
-            this.dealerScore = dealerScore;
-            this.playerHand = playerHand;
-            this.playerScore = playerScore;
-            this.dealer = new Dealer(dealerHand, dealerScore);
-            this.player = new randomPlayer(playerHand, playerScore);
-            getNumHands();
-        }
-
-    private void getNumHands() {
-        System.out.println("How many hands would you like to play?");
-        Scanner in = new Scanner(System.in);
-        int numHands = in.nextInt();
-        this.successes = new ArrayList<Integer>(numHands);
+    public static void main(String[] args) {
+        successes = new ArrayList<Double>();
+        getNumHands();
+        count = 0;
+        success = 0;
+        trackSuccess();
+        outcome();
     }
 
-    public boolean dealerWin() {
-            if (dealer.getScore() > player.getScore() && dealer.getScore() <= 21){
-                return true;
-            } else {
-                return false;
+
+    private static int getNumHands() {
+        System.out.println("How many hands would you like to play?");
+        Scanner in = new Scanner(System.in);
+        numHands = in.nextInt();
+        return numHands;
+    }
+
+    public static void trackSuccess() {
+        System.out.println(numHands);
+        for(int i = 0; i < numHands;i++){
+            RandomPlayGame rando = new RandomPlayGame();
+            rando.blackjack();
+            if (rando.isWon()) {
+                success+=1;
+                count+=1;
+                double rate = success/count;
+                successes.add(rate);
+            }
+            else{
+                count+=1;
+                double rate = success/count;
+                successes.add(rate);
             }
         }
+    }
 
-        public void trackSuccess() {
-            while(count < this.successes.size()){
-                if (!dealerWin()) {
-                    success+=1;
-                    count+=1;
-                    this.successes.add(success/count);
-                }
-                else{
-                    count+=1;
-                    this.successes.add(success/count);
-                }
-            }
-            outcome();
-        }
-
-        public ArrayList<Integer> outcome(){
-            return this.successes;
-        }
+    public static ArrayList<Double> outcome(){
+        System.out.println(successes);
+        return successes;
+    }
 }
