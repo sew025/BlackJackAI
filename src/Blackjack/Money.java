b0e8fab9090348c10ab2ab0d0a45b8d608059848
Jack Otto <jno003@bucknell.edu>
2019-12-02 11:37:50 -0500

    javadocs

--- src/Blackjack/Money.java
+++ src/Blackjack/Money.java
@@ -18,9 +18,19 @@
  */
 package Blackjack;
 
+/**
+ * Class to make a money amount for the player to have, makes the game more functional to allow the user to play with money
+ */
 public class Money {
+    /**
+     * the total amount of money being played with
+     */
     private double amount;
 
+    /**
+     * the constructor setting the money amount
+     * @author Jack Otto
+     */
     public Money() {
         this.amount = 100.00;
     }
@@ -33,14 +43,29 @@ public class Money {
         this.amount = amount;
     }
 
+    /**
+     * remove a value of money from the total
+     * @param number - the amount to decrease
+     * @author Jack Otto
+     */
     public void removeFunds(double number){
         setAmount(getAmount()-number);
     }
 
+    /**
+     * add a value of money into the total
+     * @param number - the amount of money to increase
+     * @author Jack Otto
+     */
     public void addFunds(double number){
         setAmount(getAmount()+number);
     }
 
+    /**
+     * show the amount of money the user has
+     * @return  a string showing the total with a dollar amount
+     * @author Jack Otto
+     */
     @Override
     public String toString() {
         return "Total: $" + getAmount();
